d10aedc1c209e9225eb34199c4c8678653a998b3
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-02 14:47:13 +0000

    Add: 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,8 +1,38 @@
-# Config for nginx server
+# Config for installing nginx server using puppet
 
-exec {'nginx':
-       command => '/bin/apt-get -y update',
-       command => '/bin/apt-get -y install nginx',
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+}
+
+# Configure Nginx by modifying the default site configuration
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => 'file',
+  content => <<EOT,
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location /redirect_me {
+        return 301 https://twitter.com/;
     }
+}
+EOT
+  require => Package['nginx'], # Ensure Nginx package is installed before applying this configuration
+}
 
-
+# Ensure Nginx service is running and reload the configuration
+service { 'nginx':
+  ensure     => 'running',
+  enable     => true,
+  subscribe  => File['/etc/nginx/sites-enabled/default'],
+}
