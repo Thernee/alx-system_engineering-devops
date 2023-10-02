d6d2397ccab08bab442d8d5257fb2b02f769ada9
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-02 15:18:25 +0000

    Fixed: 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -12,8 +12,7 @@ file { '/var/www/html/index.html':
 # Configure Nginx by modifying the default site configuration
 file { '/etc/nginx/sites-enabled/default':
   ensure  => 'file',
-  content => <<EOT,
-server {
+  content => "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     root /var/www/html;
@@ -25,14 +24,14 @@ server {
     location /redirect_me {
         return 301 https://twitter.com/;
     }
-}
-EOT
+}\n",
   require => Package['nginx'], # Ensure Nginx package is installed before applying this configuration
 }
 
-# Ensure Nginx service is running and reload the configuration
+# Ensure Nginx service is running and reload the configuration when changed
 service { 'nginx':
   ensure     => 'running',
   enable     => true,
   subscribe  => File['/etc/nginx/sites-enabled/default'],
 }
+
