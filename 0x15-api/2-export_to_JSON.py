0a45518aa725bec7047f30ced9fc3bb4001e261c
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-23 17:12:14 +0000

    Add: 2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Return info about employee's TODO list progress"""
+
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    baseUrl = 'https://jsonplaceholder.typicode.com/users'
+    fullUrl = f"{baseUrl}/{argv[1]}"
+
+    user = requests.get(fullUrl).json()
+    name = user.get('username')
+
+    user_id = argv[1]
+    todosUrl = f"{fullUrl}/todos"
+    todos = requests.get(todosUrl).json()
+
+    with open(f"{user_id}.json", 'w') as json_file:
+        todo_list = [{
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": name
+        } for todo in todos]
+
+        data = {user_id: todo_list}
+        json.dump(data, json_file)
