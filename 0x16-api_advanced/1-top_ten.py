33ecf7749465c2936f9fef42a24758a71e3f45eb
Abubakar Adam Sani <Athernee@gmail.com>
2023-11-07 20:03:55 +0000

    Added: 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for top_ten()."""
+import requests
+
+
+def top_ten(subreddit):
+    """"prints the titles of the first 10 hot posts listed for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/top.json?limit=10".format(subreddit)
+
+    response = requests.get(url, headers={'User-agent': 'MyLinuxApp/1.0'},
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print(None)
+    data = response.json().get('data', {}).get('children', [])
+    for post in data:
+        title = post.get('data', {}).get('title')
+        print(title)
