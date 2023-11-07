dc42f90631056b5ab9061b3dfb4da914ebc6e720
Abubakar Adam Sani <Athernee@gmail.com>
2023-11-07 21:15:35 +0000

    Added: 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,16 +3,20 @@
 import requests
 
 
-def top_ten(subreddit):
+def recurse(subreddit, hot_list=[], after=None):
     """prints the titles of 10 hot posts listed for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/top.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/top.json?after={}".format(subreddit, after)
 
     response = requests.get(url, headers={'User-agent': 'MyLinuxApp/1.0'},
                             allow_redirects=False)
     if response.status_code >= 300:
-        print(None)
-        return
+        return None
+
     data = response.json().get('data').get('children')
     for post in data:
         title = post.get('data').get('title')
-        print(title)
+        hot_list.append(title)
+    after = response.json().get('data').get('after')
+    if after is None:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
