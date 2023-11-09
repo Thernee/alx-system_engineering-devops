e02dc0f534876931b2fa3db0ad26f2a3bb243f7d
Abubakar Adam Sani <Athernee@gmail.com>
2023-11-09 17:15:30 +0000

    fixed: 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,7 +5,7 @@ import requests
 
 def count_words(subreddit, word_list, after=None, hots_dict={}):
     """print sorted count of given keywords of hot articles."""
-    url = "https://www.reddit.com/r/{}/top.json?after={}".format(
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
                 subreddit, after)
 
     response = requests.get(url, headers={'User-agent': 'MyLinuxApp/1.0'},
@@ -14,14 +14,13 @@ def count_words(subreddit, word_list, after=None, hots_dict={}):
         return None
 
     data = response.json().get('data').get('children')
+    after = response.json().get('data').get('after')
     for post in data:
         title = post.get('data').get('title')
         lower_list = [hot.lower() for hot in title.split(' ')]
 
         for hot in hots_dict.keys():
             hots_dict[hot] += lower_list.count(hot)
-
-    after = response.json().get('data').get('after')
 
     if after is None:
         word_items = hots_dict.items()
