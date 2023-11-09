bb45d4c62be66541be46659bf64aeffc2aed647d
Abubakar Adam Sani <Athernee@gmail.com>
2023-11-09 16:55:42 +0000

    Added: 100-count.py for advanced tasks

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module for count_words()."""
+import requests
+
+
+def count_words(subreddit, word_list, after=None, hots_dict={}):
+    """print sorted count of given keywords of hot articles."""
+    url = "https://www.reddit.com/r/{}/top.json?after={}".format(
+                subreddit, after)
+
+    response = requests.get(url, headers={'User-agent': 'MyLinuxApp/1.0'},
+                            allow_redirects=False)
+    if response.status_code >= 300:
+        return None
+
+    data = response.json().get('data').get('children')
+    for post in data:
+        title = post.get('data').get('title')
+        lower_list = [hot.lower() for hot in title.split(' ')]
+
+        for hot in hots_dict.keys():
+            hots_dict[hot] += lower_list.count(hot)
+
+    after = response.json().get('data').get('after')
+
+    if after is None:
+        word_items = hots_dict.items()
+        sorted_word_dict = sorted(word_items, key=lambda x: (-x[1], x[0]))
+
+        for word in sorted_word_dict:
+            if word[1]:
+                print('{}: {}'.format(word[0], word[1]))
+        return None
+
+    if not hots_dict:
+        for word in word_list:
+            if word.lower() not in hots_dict:
+                hots_dict[word.lower()] = 0
+
+    count_words(subreddit, word_list, after, hots_dict)
